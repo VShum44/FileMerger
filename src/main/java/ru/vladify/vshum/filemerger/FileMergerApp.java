ee7bae6c86d05bd390077b99863ec37002247652
Vlad <vshum44@yandex.ru>
2026-03-08 13:08:31 +0300

    init commit

---
+++ src/main/java/ru/vladify/vshum/filemerger/FileMergerApp.java
@@ -0,0 +1,27 @@
+package ru.vladify.vshum.filemerger;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class FileMergerApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("JavaFX работает!");
+        label.setStyle("-fx-font-size: 24px;");
+
+        VBox root = new VBox(label);
+        Scene scene = new Scene(root, 400, 200);
+
+        primaryStage.setTitle("Тест JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/ru/vladify/vshum/filemerger/Launcher.java
@@ -0,0 +1,7 @@
+package ru.vladify.vshum.filemerger;
+
+public class Launcher {
+    public static void main(String[] args) {
+        FileMergerApp.main(args);
+    }
+}
