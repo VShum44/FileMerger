c8fab98ab52575ea950d12c8efe6747650816b6a
Vlad <vshum44@yandex.ru>
2026-03-20 17:51:37 +0300

    save main window position after app reload

--- src/main/java/ru/vladify/vshum/filemerger/FileMergerApp.java
+++ src/main/java/ru/vladify/vshum/filemerger/FileMergerApp.java
@@ -9,8 +9,11 @@ import ru.vladify.vshum.filemerger.controller.MainController;
 import ru.vladify.vshum.filemerger.util.DialogHelper;
 
 import java.io.IOException;
+import java.util.prefs.Preferences;
 
 public class FileMergerApp extends Application {
+
+    private final Preferences prefs = Preferences.userNodeForPackage(getClass());
 
     /**
      * Запускает приложение — загружает FXML, подключает CSS,
@@ -29,6 +32,8 @@ public class FileMergerApp extends Application {
             throw new RuntimeException("Проблема загрузки сцены");
         }
 
+
+
         MainController mainController = loader.getController();
         primaryStage.setOnCloseRequest(event -> {
             if(!mainController.isFilesEmpty()){
@@ -44,6 +49,31 @@ public class FileMergerApp extends Application {
         // Подключаем CSS
         scene.getStylesheets().add(getClass().getResource("style/style.css").toExternalForm());
         scene.getStylesheets().add(getClass().getResource("style/theme-light.css").toExternalForm());
+
+        // Восстанавливаем положение и размер окна
+        double x = prefs.getDouble("window_x", 100);
+        double y = prefs.getDouble("window_y", 100);
+        double width = prefs.getDouble("window_width", 800);
+        double height = prefs.getDouble("window_height", 600);
+
+        primaryStage.setX(x);
+        primaryStage.setY(y);
+        primaryStage.setWidth(width);
+        primaryStage.setHeight(height);
+
+        // Сохраняем положение при закрытии
+        primaryStage.setOnCloseRequest(event -> {
+            prefs.putDouble("window_x", primaryStage.getX());
+            prefs.putDouble("window_y", primaryStage.getY());
+            prefs.putDouble("window_width", primaryStage.getWidth());
+            prefs.putDouble("window_height", primaryStage.getHeight());
+
+            try {
+                prefs.flush(); // Принудительно сохраняем
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 
         primaryStage.setTitle(AppConfig.APP_TITLE);
         primaryStage.setScene(scene);
